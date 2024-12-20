b4bfaf3636def2be9e73c7ef74b79baa8d873be1
youssifayman777 <yshehab693@gmail.com>
2024-12-20 15:53:40 +0200

    Hello

---
+++ yussuff.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{   
+   cout<<"YOUSSIf Ayman Shehab \n";
+    int x,y;
+    cout<<"Enter two numbers"; 
+   cin>>x>>y;
+    cout<<"Sum = " <<x+y;   
+
+}
